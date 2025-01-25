b5725d2245dbb7a4f73f0938b62ce26bef7bc075
José Luis Bautista Rodríguez <joseluisbaur@gmail.com>
2025-01-25 16:03:35 -0600

    Svelte changes

--- 01_webApp/labconnect-smc/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "svelte-spa-router";
-import { routes } from "./routes.js";
-
-const app = new Router({
-  target: document.body,
-  routes,
-});
-
-export default app;
--- 01_webApp/labconnect-smc/scripts/routes.js
+++
@@ -1,7 +0,0 @@
-import App from "./App.svelte";
-import VentanaSecundaria from "./VentanaSecundaria.svelte";
-
-export const routes = {
-  "/": App,
-  "/ventana-secundaria": VentanaSecundaria,
-};
